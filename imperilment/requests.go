4539b8d042e078dd1ad0076e8a34a645f1cf30d0
Ricahrd Wilson <r.crawfordwilson@gmail.com>
2014-05-27 18:52:28 -0700

    Allow getBoard to receive a dynamic type

--- imperilment/requests.go
+++ imperilment/requests.go
@@ -8,24 +8,26 @@ import (
 )
 
 func (e *Engine) GetOverallLeaderBoard() (board []LeaderBoardEntry, ok bool) {
-    return e.getBoard(e.OverallLeaderBoardUrl)
+    ok = e.getBoard(e.OverallLeaderBoardUrl, &board)
+    return board, ok
 }
 
 func (e *Engine) GetMoneyLeaderBoard() (board []LeaderBoardEntry, ok bool) {
-    return e.getBoard(e.MoneyLeaderBoardUrl)
+    ok = e.getBoard(e.MoneyLeaderBoardUrl, &board)
+    return board, ok
 }
 
-func (e *Engine) getBoard(url string) (board []LeaderBoardEntry, ok bool) {
+func (e *Engine) getBoard(url string, board interface{}) (ok bool) {
     response, err := http.Get(url)
     if err != nil {
         log.Fatal(err)
-        return board, false
+        return false
     }
     defer response.Body.Close()
 
     body, _ := ioutil.ReadAll(response.Body)
 
-    json.Unmarshal(body, &board)
-    return board, true
+    json.Unmarshal(body, board)
+    return true
 }
 
